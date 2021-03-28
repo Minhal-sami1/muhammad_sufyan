6b72d1ada16199e9d0f2febdc63288ca853f6792
Minhal-sami1 <romeomino415@gmail.com>
2021-03-28 14:01:38 +0500

    deployam

--- blog/urls.py
+++ blog/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path("search", views.search, name = "sf")
 
 
-] 
-if settings.DEBUG:
-     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
